f21ad5eac3f52f87110a2ea40a118859cd66abd7
Senpat <Senpat@users.noreply.github.com>
2017-09-16 12:00:22 -0400

    Create hibot.py

---
+++ hibot.py
@@ -0,0 +1,14 @@
+#says hi when someone says hi to me
+
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+import praw
+import time
+import re
+import requests
+import bs4
+
+def authenticate():
+  print("Authenticating...\n")
+  
