848d8a50d43b304dac5b775cfdd17ed73f250c0b
Senpat <Senpat@users.noreply.github.com>
2017-09-16 12:37:39 -0400

    Update hibot.py

--- hibot.py
+++ hibot.py
@@ -11,5 +11,29 @@ import bs4
 
 def authenticate():
   print("Authenticating...\n")
-  
-  
+  reddit = praw.Reddit('hibot',user_agent = 'web:hi-bot:v0.1 (by /u/The_Senpat)')
+  print('Authenticated as {}\n'.format(reddit.user.me()))
+  return reddit
+
+def run_hibot(reddit):
+    print('getting 250 comments...\n')
+    
+    for comment in reddit.subreddit('test').comments(limit = 250):
+        match = re.findall("hi /u/TheSenpat",comment.body)
+        if match:
+            print('Link found with comment ID: " + comment.id')
+            
+            #check to see if comment id is in the file
+            
+            comment.reply('hi')
+            
+            time.sleep(60)
+            
+            
+def main():
+    reddit = authenticate()
+    while True:
+        run_hibot(reddit)
+      
+if __name == '__main__':
+    main()
